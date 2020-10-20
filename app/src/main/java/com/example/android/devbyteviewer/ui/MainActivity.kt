fb2e8509d25197f25f18a162c3bbf72d2ab2c05d
eggham0518 <eggham0518@gmail.com>
2020-10-20 15:14:26 +0900

    loopRecyclerview

--- app/src/main/java/com/example/android/devbyteviewer/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/devbyteviewer/ui/MainActivity.kt
@@ -23,6 +23,8 @@ import com.example.android.devbyteviewer.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_dev_byte_viewer)
     }
 }
